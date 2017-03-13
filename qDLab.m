b858638731fc5bc88575465e7030929929983fb6
tanguyduval <duvaltanguy@gmail.com>
2017-03-13 16:34:16 -0400

    Update qDLab.m

--- qDLab.m
+++ qDLab.m
@@ -546,7 +546,7 @@ Ax.scheme=handles.scheme(logical(handles.Selection),:);
 Ax.Dr=str2double(get(handles.Dr,'String'));
 Ax.Dcsf=str2double(get(handles.Dcsf,'String'));
 
-%if ~get(handles.csf,'Value'), Ax.Dcsf=0; end
+if ~get(handles.csf,'Value'), Ax.Dcsf=0; end
 
 handles.x =[];
 % norm
